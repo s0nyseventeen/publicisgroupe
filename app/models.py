6f932242ed06f4f03e547c7a8085c5c0c85711bd
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-10-25 22:56:30 +0300

    Add UploadedData class in order to create uploadeddata table in the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlmodel import Field
 from sqlmodel import Relationship
 from sqlmodel import SQLModel
@@ -9,3 +11,16 @@ class UploadedFile(SQLModel, table=True):
     uploaded_data: list['UploadedData'] = Relationship(
         back_populates='uploaded_file'
     )
+
+
+class UploadedData(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    uploaded_file_id: int = Field(foreign_key='uploadedfile.id')
+    advertiser: str = Field(nullable=False)
+    brand: str = Field(nullable=False)
+    start: date = Field(nullable=False)
+    end: date = Field(nullable=False)
+    format: str = Field(nullable=False)
+    platform: str = Field(nullable=False)
+    impr: float = Field(nullable=False)
+    uploaded_file: UploadedFile = Relationship(back_populates='uploaded_data')
