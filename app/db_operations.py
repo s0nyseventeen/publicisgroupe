435250416623b40fb47faf7a2f0211697a7d4bf8
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-10-25 22:06:39 +0300

    Implement create_uploaded_data function in order to create UploadedData record in the database

--- app/db_operations.py
+++ app/db_operations.py
@@ -1,5 +1,7 @@
+import pandas as pd
 from sqlmodel import Session
 
+from .models import UploadedData
 from .models import UploadedFile
 
 
@@ -9,3 +11,21 @@ def create_uploaded_file(file_name: str, session: Session) -> UploadedFile:
     session.commit()
     session.refresh(uploaded_file)
     return uploaded_file
+
+
+def create_uploaded_data(
+    df: pd.DataFrame, uploaded_file_id: int, session: Session
+):
+    for _, row in df.iterrows():
+        uploaded_data = UploadedData(
+            uploaded_file_id=uploaded_file_id,
+            advertiser=row['Advertiser'],
+            brand=row['Brand'],
+            start=row['Start'],
+            end=row['End'],
+            format=row['Format'],
+            platform=row['Platform'],
+            impr=row['Impr']
+        )
+        session.add(uploaded_data)
+    session.commit()
