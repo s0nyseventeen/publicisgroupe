01b0e16a25bd5a0dfdc5b6ddf3641109bd183c4d
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-10-25 22:55:28 +0300

    Implement read_file function in order validate file format

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+import pandas as pd
 from fastapi import FastAPI
+from fastapi import UploadFile
 
 app = FastAPI()
+
+
+def read_file(file: UploadFile) -> pd.DataFrame:
+    file_name = file.filename
+    if not file_name.endswith(('.csv', '.xls', '.xlsx')):
+        raise ValueError('Unsupported file format')
+    if file_name.endswith('.csv'):
+        return pd.read_csv(file.file)
+    return pd.read_excel(file.file)
 
 
 @app.post("/upload")
